a51c060688bd1dba162ff5b9da5d60c9622650bb
alexmerlo0623 <a.merlo@ufl.edu>
2025-03-23 13:24:19 -0400

    Cleaned up sarnat exam part 5

--- frontend/src/pages/RightBranch/RightBranch.jsx
+++ frontend/src/pages/RightBranch/RightBranch.jsx
@@ -99,32 +99,36 @@ export default function RightBranch() {
             <div className="right-branch-sarnat-exam">
                 <h1>Simple Sarnat Exam</h1>
                 <h2>1. Gestational Age</h2>
-                {renderRadioGroup("gestational_age", "Gestational Age ≥ 35 weeks", ["Yes", "No"])}
+                {renderRadioGroup("gestational_age", "Is the gestational age of the neonate ≥ 35 weeks?", ["Yes", "No"])}
                 
                 <h2>2. Birth Weight</h2>
-                {renderRadioGroup("birth_weight", "Birth Weight ≥ 1.8 kg", ["Yes", "No"])}
+                {renderRadioGroup("birth_weight", "Is the birth weight ≥ 1.8 kg?", ["Yes", "No"])}
                 
                 <h2>3. Time Since Insult</h2>
-                {renderRadioGroup("time_since_insult", "Time Since Insult ≤ 6 hours", ["Yes", "No"])}
+                {renderRadioGroup("time_since_insult", "Has it been ≤ 6 hours since the last insult occured?", ["Yes", "No"])}
                 
                 <h2>4. ONE OR MORE Predictors of Severe HIE</h2>
-                <label>pH:</label>
-                <input
-                    type="number"
-                    value={Ph}
-                    onChange={(e) => setPh(e.target.value)}
-                    placeholder="Enter pH"
-                    step="0.01"
-                />
-                <label>Base Deficit:</label>
-                <input
-                    type="number"
-                    value={BaseDeficit}
-                    onChange={(e) => setBaseDeficit(e.target.value)}
-                    placeholder="Enter Base Deficit"
-                    step="0.01"
-                    
-                />
+                <div className="input-group">
+                    <label className="phLabel">Enter pH:</label>
+                    <input
+                        type="number"
+                        value={Ph}
+                        onChange={(e) => setPh(e.target.value)}
+                        placeholder="pH"
+                        step="0.01"
+                    />
+                </div>
+                <div className="input-group">
+                    <label className="baseDeficitLabel">Enter Base Deficit:</label>
+                    <input
+                        type="number"
+                        value={BaseDeficit}
+                        onChange={(e) => setBaseDeficit(e.target.value)}
+                        placeholder="Base Deficit"
+                        step="0.01"
+                    />
+                </div>
+
                 <h3>Predictor A</h3>
                 {renderHighlightGroup(
                   "predictor_a",
@@ -132,7 +136,7 @@ export default function RightBranch() {
                  parseFloat(Ph) <= 7.0 && parseFloat(BaseDeficit) >= 16,
                  !(parseFloat(Ph) <= 7.0 && parseFloat(BaseDeficit) >= 16)
                 )}
-                <h3>Predictor B pH 7.01 - 7.15 with base deficit 10-15.9</h3>
+                <h3>Predictor B</h3>
                 {renderHighlightGroup(
                  "predictor_b",
                  "pH 7.01 - 7.15 with base deficit 10-15.9",
@@ -142,20 +146,30 @@ export default function RightBranch() {
                 <h3>Predictor C (if no blood gas available)</h3>
                 {renderRadioGroup("acute_perinatal_event", "Acute perinatal event (cord prolapse, heart rate decelerations, uterine rupture)", ["Yes", "No"])}
                 <h4>and</h4>
-                {renderRadioGroup("apgar_assisted_ventilation", "APGAR ≤ 5 at 10 minutes or assisted ventilation at birth required ≥ 10 min", ["Yes", "No"])}
+                {renderRadioGroup("apgar_assisted_ventilation", "APGAR ≤ 5 at 10 minutes or assisted ventilation at birth required ≥ 10 minutes", ["Yes", "No"])}
 
                 <h2>5. Has seizures or 3 of 6 of the following signs of Encephalopathy:</h2>
-                {renderRadioGroup("level_of_consciousness", "Level of Consciousness", ["Lethargic", "Stupor / Coma"])}
-                {renderRadioGroup("spontaneous_activity", "Spontaneous Activity", ["Decreased", "No Activity"])}
-                {renderRadioGroup("posture", "Posture", ["Distal Flexion / Extension", "Decerebrate"])}
-                {renderRadioGroup("tone", "Tone", ["Hypotonia / Hypertonia", "Flaccid"])}
-                {renderRadioGroup("suck", "Primitive Reflexes (Suck)", ["Weak/Bite", "Absent"])}
-                {renderRadioGroup("moro", "Primitive Reflexes (Moro)", ["Incomplete", "Absent"])}
-                {renderRadioGroup("pupils", "Autonomic System (Pupils)", ["Constricted", "Skew / Non-reactive"])}
-                {renderRadioGroup("heart_rate", "Heart Rate", ["Bradycardia", "Variable"])}
-                {renderRadioGroup("respirations", "Respirations", ["Periodic", "Apnea/Intubated"])}
-                <button onClick={calculateScore}>Calculate Score</button>
-                <h3>{result}</h3>
+                <div className="part-5-container">
+                    <div className="part-5-description-container">
+                        <div className="part-5-description-left">
+                            <p>Clinical Criteria</p>
+                        </div>
+                        <div className="part-5-description-right">
+                            <p>Left Indicates Moderate | Right Indicates Severe.</p>
+                        </div>
+                    </div>
+                    {renderRadioGroup("level_of_consciousness", "Level of Consciousness", ["Lethargic", "Stupor / Coma"])}
+                    {renderRadioGroup("spontaneous_activity", "Spontaneous Activity", ["Decreased", "No Activity"])}
+                    {renderRadioGroup("posture", "Posture", ["Distal Flexion / Extension", "Decerebrate"])}
+                    {renderRadioGroup("tone", "Tone", ["Hypotonia / Hypertonia", "Flaccid"])}
+                    {renderRadioGroup("suck", "Primitive Reflexes (Suck)", ["Weak/Bite", "Absent"])}
+                    {renderRadioGroup("moro", "Primitive Reflexes (Moro)", ["Incomplete", "Absent"])}
+                    {renderRadioGroup("pupils", "Autonomic System (Pupils)", ["Constricted", "Skew / Non-reactive"])}
+                    {renderRadioGroup("heart_rate", "Heart Rate", ["Bradycardia", "Variable"])}
+                    {renderRadioGroup("respirations", "Respirations", ["Periodic", "Apnea/Intubated"])}
+                </div>
+            <button onClick={calculateScore}>Calculate Score</button>
+            <h3>{result}</h3>
             </div>
         </div>
     );

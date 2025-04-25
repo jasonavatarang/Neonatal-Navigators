a87030540289fdeb82f305a5ed75d3224b980460
alexmerlo0623 <a.merlo@ufl.edu>
2025-04-25 11:56:46 -0400

    Remove unused files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Header from "./assets/Header"; // Import the new Header component
-import Home from "./pages/Home/Home";
-import LandingPage from "./pages/LandingPage/LandingPage";
-import LandingLeftBranch from "./pages/LandingLeftBranch/LandingLeftBranch";
-import LandingRightBranch from "./pages/LandingRightBranch/LandingRightBranch";
-import LeftBranch from "./pages/LeftBranch/LeftBranch";
-import MidBranch from "./pages/MidBranch/MidBranch";
+import Header from "./assets/Header";
 import RightBranch from "./pages/RightBranch/RightBranch";
 
 export default function App() {
@@ -14,14 +8,7 @@ export default function App() {
         <>
         <Header />
         <Routes>
-            {/*<Route path="/" element={<LandingPage />} />*/}
-            <Route path="/landingleftbranch" element={<LandingLeftBranch />} />
-            <Route path="/landingrightbranch" element={<LandingRightBranch />} />
-            {/*<Route path="/" element={<Home />} />*/}
             <Route path="/" element={<RightBranch />} />
-            <Route path="/leftbranch" element={<LeftBranch />} />
-            <Route path="/midbranch" element={<MidBranch />} />
-            <Route path="/rightbranch" element={<RightBranch />} />
         </Routes>
         </>
     );
